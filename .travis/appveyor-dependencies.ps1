2e121b286b8231e1ebef9fa89d609ccedf297d58
tooomm <tooomm@users.noreply.github.com>
2018-01-24 00:05:40 +0100

    script test

---
+++ .travis/appveyor-dependencies.ps1
@@ -0,0 +1,18 @@
+if (Test-Path c:\protobuf-release) {
+    echo "using protobuf from cache"
+} else {
+    Invoke-WebRequest "https://github.com/google/protobuf/releases/download/v$env:protobuf_ver/protobuf-cpp-$env:protobuf_ver.zip" -OutFile c:\protobuf-cpp-$env:protobuf_ver.zip
+    Expand-Archive -Path c:\protobuf-cpp-$env:protobuf_ver.zip -DestinationPath c:\
+    Set-Location -Path C:\protobuf-$env:protobuf_ver\cmake
+    cmake . -G "$env:cmake_generator" -T "$env:cmake_toolset" -Dprotobuf_BUILD_TESTS=0 -Dprotobuf_MSVC_STATIC_RUNTIME=0 -DCMAKE_INSTALL_PREFIX=c:/protobuf-release
+    msbuild INSTALL.vcxproj /p:Configuration=Release
+}
+if (Test-Path c:\zlib-release) {
+    echo "using zlib from cache"
+} else {
+    Invoke-WebRequest "https://github.com/madler/zlib/archive/v$env:zlib_ver.zip" -OutFile c:\zlib-$env:zlib_ver.zip
+    Expand-Archive -Path c:\zlib-$env:zlib_ver.zip -DestinationPath c:\
+    Set-Location -Path C:\zlib-$env:zlib_ver
+    cmake . -G "$env:cmake_generator" -T "$env:cmake_toolset" -DCMAKE_INSTALL_PREFIX=c:/zlib-release
+    msbuild INSTALL.vcxproj /p:Configuration=Release
+}
