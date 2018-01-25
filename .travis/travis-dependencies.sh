64f95ee969ce247e1b8f3c8e39e7a62915278a9e
tooomm <tooomm@users.noreply.github.com>
2018-01-25 22:18:43 +0100

    test mac again

--- .travis/travis-dependencies.sh
+++ .travis/travis-dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
-  brew install ccache
-  export PATH="/usr/local/opt/ccache/libexec:$PATH"
   brew install --force qt@5.7
   brew install protobuf
+  brew install ccache
+  export PATH="/usr/local/opt/ccache/libexec:$PATH"
 fi
